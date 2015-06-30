acdcf80086de6d69f040b4818e5b08e81ebf3580
Zaenardo <zaenardo@gmail.com>
2015-06-30 23:21:17 +0700

    ganti todolistservlet ke database

--- src/main/java/com/zein/belajar/servlet/TodoListServlet.java
+++ src/main/java/com/zein/belajar/servlet/TodoListServlet.java
@@ -17,42 +17,22 @@ import com.zein.belajar.dao.TodoDao;
 
 @WebServlet("/todo/list")
 public class TodoListServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse res)
-			throws IOException, ServletException {
-				
-				//data yang akan ditampilkan, nantinya ambil dari database
-				List<Todo> daftarTodo = new ArrayList<Todo>();
-				
-				
-				Todo t1= new Todo();
-				t1.setId(1);
-				t1.setKeterangan("Registrasi Openshift");
-				t1.setSelesai(true);
-				daftarTodo.add(t1);
-				
-				Todo t2= new Todo();
-				t2.setId(2);
-				t2.setKeterangan("Buat app baru");
-				t2.setSelesai(false);
-				daftarTodo.add(t2);
-				
-				Todo t3= new Todo();
-				t3.setId(3);
-				t3.setKeterangan("BDeploy Aplikasi");
-				t3.setSelesai(false);
-				daftarTodo.add(t3);
-				
-				//masukan data ke request attribute
-				req.setAttribute("todo", daftarTodo);
-				
-				
-				
-				// redirect ke tampilan
-				String lokasiJsp = "/WEB-INF/jsp/list.jsp";
-				RequestDispatcher rd = req.getRequestDispatcher(lokasiJsp);
-				rd.forward(req,res);
-				
-			}
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) 
+                                        throws IOException, ServletException {
+                                        
+        // Koneksi database
+        TodoDao td = new TodoDao();
+                                        
+        // Data yang akan ditampilkan
+        List<Todo> daftarTodo = td.semuaTodo();
+        
+        // masukkan data ke request attribute
+        req.setAttribute("todo", daftarTodo);
+        
+        // redirect ke tampilan
+        String lokasiJsp = "/WEB-INF/jsp/todo/list.jsp";
+        RequestDispatcher rd = req.getRequestDispatcher(lokasiJsp);
+        rd.forward(req, res);                         
+    }
 }
-
-
